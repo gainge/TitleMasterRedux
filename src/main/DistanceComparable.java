729be9372acfcf4fb67d749c47b142f20d31e2a3
Gibson Ainge <gainge@gmail.com>
2019-02-17 08:29:24 -0700

    Changed how I'm architecturing the comparison interface, I like things better this way

---
+++ src/main/DistanceComparable.java
@@ -0,0 +1,6 @@
+package main;
+
+public interface DistanceComparable<T> {
+
+    double getDistance(T other);
+}
---
+++ src/main/MatchupNode.java
@@ -0,0 +1,14 @@
+package main;
+
+public class MatchupNode implements DistanceComparable<MatchupNode> {
+
+    // So, this is where we'll store the actual data and whatever for the games
+
+
+
+
+    @Override
+    public double getDistance(MatchupNode other) {
+        return 0;
+    }
+}
